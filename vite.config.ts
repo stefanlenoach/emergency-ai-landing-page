749e24e3f9c2261bdac08f7b9965ec2e0a365a06
Stefan Le Noach <stefanlenoach@gmail.com>
2024-10-29 16:12:13 -0400

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    proxy: {
+      '/blog': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
+    },
+  },
 });
